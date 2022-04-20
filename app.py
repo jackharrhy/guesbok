8b3c4f5eaf55327a6586271a8daa45735daeb347
Jack Arthur Harrhy <me@jackharrhy.com>
2022-04-19 21:38:30 -0230

    guesbok

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import shelve
 from flask import Flask, request, render_template
+from jinja2 import Environment, BaseLoader
 
 app = Flask(__name__)
 
@@ -11,8 +12,16 @@ if 'entries' not in persist: persist['entries'] = []
 def index():
     return render_template('index.html', entries=persist['entries'])
 
+def handle_entries():
+    for entry in persist['entries']:
+        try:
+            template = Environment(loader=BaseLoader()).from_string(entry)
+            yield template.render()
+        except:
+            yield entry
+
 @app.route('/', methods=['POST'])
 def add_note():
     text = request.form['text']
     persist['entries'].append(text)
-    return render_template('index.html', entries=persist['entries'])
+    return render_template('index.html', entries=list(handle_entries()))
