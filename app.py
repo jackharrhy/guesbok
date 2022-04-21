98800375b48a6c10b4b3cdee4615eb2ba5ec47ef
Jack Arthur Harrhy <me@jackharrhy.com>
2022-04-20 22:45:40 -0230

    guesbok

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import json
 import os
 import shelve
+import pprint
 from flask import Flask, request, render_template
 from jinja2 import Environment, BaseLoader
 
@@ -8,20 +10,23 @@ app = Flask(__name__)
 persist = shelve.open(os.path.realpath('store.dat'), writeback=True)
 if 'entries' not in persist: persist['entries'] = []
 
-@app.route('/')
-def index():
-    return render_template('index.html', entries=persist['entries'])
+def fetch_context():
+    return {**globals(), **locals()}
 
 def handle_entries():
     for entry in persist['entries']:
         try:
             template = Environment(loader=BaseLoader()).from_string(entry)
-            yield template.render()
-        except:
+            yield template.render(**fetch_context())
+        except Exception as e:
             yield entry
+
+@app.route('/')
+def index():
+    return render_template('index.html', **fetch_context(), entries=list(handle_entries()))
 
 @app.route('/', methods=['POST'])
 def add_note():
     text = request.form['text']
     persist['entries'].append(text)
-    return render_template('index.html', entries=list(handle_entries()))
+    return render_template('index.html', **fetch_context(), entries=list(handle_entries()))
